69899cf78c71b94904f4db06dc593b5e80bea6dd
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2024-11-08 20:05:55 +0600

    passing code

---
+++ Js/main.js
@@ -0,0 +1,30 @@
+/**
+ *  to do: project
+ */
+
+let num = prompt("Enter number: ");
+let namta = +num;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(namta + " X " + i + " = " + namta * i);
+  document.write(namta + " X " + i + " = " + namta * i + "<br/>");
+
+  console.log(`${namta} X ${i} = ${namta * i}`);
+}
+
+// let x = 10;
+// while (x <= 20) {
+//   console.log(x);
+//   x++;
+// }
+
+// let text = "";
+// let i = 0;
+// do {
+//   text += i + " <br/> ";
+//   i++;
+// } while (i < 5);
+
+// console.log(text);
+
+// document.write(text);
